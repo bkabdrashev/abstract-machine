455cbabc8f1f2f33e6e6acffa7dbf0a04efc2b7e
bekzat <bkbdrsh@gmail.com>
2025-12-25 23:57:42 +0500

    Removed unsed macros

--- tools/minirv/inst-replace.h
+++ tools/minirv/inst-replace.h
@@ -48,36 +48,36 @@ _check_8bit_0_table:
 .byte 0
 .fill 255, 1, 1
 
-# slt = OF ^ SF = ((sign(A) ^ sign(B)) & (sign(A) ^ sign(R))) ^ sign(R)
-# sltu = slt ^ sign(A) ^ sign(B)
-# A  B  R  OF  slt  sltu (sltu = slt ^ A ^ B)
-# 0  0  0  0    0    0
-# 0  0  1  0    1    1
-# 0  1  0  0    0    1
-# 0  1  1  1    0    1
-# 1  0  0  1    1    0
-# 1  0  1  0    1    0
-# 1  1  0  0    0    0
-# 1  1  1  0    1    1
-.weak _slt1_table
-_slt1_table: .byte 0, 1, 0, 0, 1, 1, 0, 1
-.weak _sltu1_table
-_sltu1_table: .byte 0, 1, 1, 1, 0, 0, 0, 1
+// # slt = OF ^ SF = ((sign(A) ^ sign(B)) & (sign(A) ^ sign(R))) ^ sign(R)
+// # sltu = slt ^ sign(A) ^ sign(B)
+// # A  B  R  OF  slt  sltu (sltu = slt ^ A ^ B)
+// # 0  0  0  0    0    0
+// # 0  0  1  0    1    1
+// # 0  1  0  0    0    1
+// # 0  1  1  1    0    1
+// # 1  0  0  1    1    0
+// # 1  0  1  0    1    0
+// # 1  1  0  0    0    0
+// # 1  1  1  0    1    1
+// .weak _slt1_table
+// _slt1_table: .byte 0, 1, 0, 0, 1, 1, 0, 1
+// .weak _sltu1_table
+// _sltu1_table: .byte 0, 1, 1, 1, 0, 0, 0, 1
 
-.weak _or1_table
-_or1_table: .byte 0, 1, 1, 1
+// .weak _or1_table
+// _or1_table: .byte 0, 1, 1, 1
 
-.weak _logic_shift_table
-.align 8
-_logic_shift_table:
-.incbin _LUT_BIN_PATH  # defined in command line flags
+// .weak _logic_shift_table
+// .align 8
+// _logic_shift_table:
+// .incbin _LUT_BIN_PATH  # defined in command line flags
 
-#define _and8_table _logic_shift_table
-#define _or8_table  (_and8_table + 256 * 256)
-#define _xor8_table (_or8_table  + 256 * 256)
-#define _sll8_table (_xor8_table + 256 * 256)
-#define _srl8_table (_sll8_table + 32 * 256 * 4)
-#define _sra8_table (_srl8_table + 32 * 256 * 4)
+// #define _and8_table _logic_shift_table
+// #define _or8_table  (_and8_table + 256 * 256)
+// #define _xor8_table (_or8_table  + 256 * 256)
+// #define _sll8_table (_xor8_table + 256 * 256)
+// #define _srl8_table (_sll8_table + 32 * 256 * 4)
+// #define _sra8_table (_srl8_table + 32 * 256 * 4)
 
 #define def_itype(name, rtype_name) \
   .macro name rd, rs1, imm ;\
@@ -109,12 +109,12 @@ _check_same_array: .fill 256, 1, 0
   .endif
 .endm
 
-.macro sub rd, rs1, rs2
-  SET_DEBUG_LABEL(sub)
-  not tp, \rs2
-  add \rd, \rs1, tp
-  addi \rd, \rd, 1
-.endm
+// .macro sub rd, rs1, rs2
+//   SET_DEBUG_LABEL(sub)
+//   not tp, \rs2
+//   add \rd, \rs1, tp
+//   addi \rd, \rd, 1
+// .endm
 
 .macro getbyte rd, rs1, byte
   sw \rs1, SP_VAR(VAR_C)
@@ -127,42 +127,42 @@ _check_same_array: .fill 256, 1, 0
   getbyte \rd, \rd, 1
 .endm
 
-.macro slt_template lut, rd, rs1, rs2
-  PUSH(s0, 1)
-  PUSH(s1, 2)
-  sw \rs2, SP_VAR(VAR_A)
-  mv s0, \rs1
-  lw s1, SP_VAR(VAR_A)
+// .macro slt_template lut, rd, rs1, rs2
+//   PUSH(s0, 1)
+//   PUSH(s1, 2)
+//   sw \rs2, SP_VAR(VAR_A)
+//   mv s0, \rs1
+//   lw s1, SP_VAR(VAR_A)
 
-  msb gp, s0
-  slli gp, gp, 1
+//   msb gp, s0
+//   slli gp, gp, 1
 
-  msb tp, s1
-  add gp, gp, tp
-  slli gp, gp, 1
+//   msb tp, s1
+//   add gp, gp, tp
+//   slli gp, gp, 1
 
-  sub tp, s0, s1 # sub uses tp
-  msb tp, tp
-  add gp, gp, tp
+//   sub tp, s0, s1 # sub uses tp
+//   msb tp, tp
+//   add gp, gp, tp
 
-  la tp, \lut
-  add tp, tp, gp
+//   la tp, \lut
+//   add tp, tp, gp
 
-  POP(s1, 2)
-  POP(s0, 1)
+//   POP(s1, 2)
+//   POP(s0, 1)
 
-  lbu \rd, (tp)
-.endm
+//   lbu \rd, (tp)
+// .endm
 
-.macro slt rd, rs1, rs2
-  SET_DEBUG_LABEL(slt)
-  slt_template _slt1_table, \rd, \rs1, \rs2
-.endm
+// .macro slt rd, rs1, rs2
+//   SET_DEBUG_LABEL(slt)
+//   slt_template _slt1_table, \rd, \rs1, \rs2
+// .endm
 
-.macro sltu rd, rs1, rs2
-  SET_DEBUG_LABEL(slt)
-  slt_template _sltu1_table, \rd, \rs1, \rs2
-.endm
+// .macro sltu rd, rs1, rs2
+//   SET_DEBUG_LABEL(slt)
+//   slt_template _sltu1_table, \rd, \rs1, \rs2
+// .endm
 
 .macro logic8_byte_internal lut_reg, boffset
   lbu tp, SP_VAR_BYTE(VAR_A, \boffset)
@@ -207,9 +207,9 @@ _check_same_array: .fill 256, 1, 0
     logic concat(_, concat(name, 8_table)), \rd, \rs1, \rs2; \
   .endm
 
-def_logic(and)
-def_logic(or)
-def_logic(xor)
+// def_logic(and)
+// def_logic(or)
+// def_logic(xor)
 
 
 .macro check_8bit_same table, rd, boffset  # rd == 0 if same
@@ -225,6 +225,7 @@ def_logic(xor)
   lbu \rd, (tp)
 .endm
 
+// TODO: remove when BEQ, BNE are implemented
 .macro seq rd, rs1, rs2   # set equal
   sw \rs1, SP_VAR(VAR_A)
   sw \rs2, SP_VAR(VAR_B)
@@ -254,25 +255,25 @@ def_logic(xor)
   lbu \rd, (\rd)
 .endm
 
-.macro seqz rd, rs1   # set if 0
-  sw \rs1, SP_VAR(VAR_A)
-  PUSH(s0, 1)
+// .macro seqz rd, rs1   # set if 0
+//   sw \rs1, SP_VAR(VAR_A)
+//   PUSH(s0, 1)
 
-  la s0, _check_8bit_0_table
-  check_8bit_0 s0, gp, 0
-  check_8bit_0 s0, tp, 1
-  add gp, gp, tp
-  check_8bit_0 s0, tp, 2
-  add gp, gp, tp
-  check_8bit_0 s0, tp, 3
-  add gp, gp, tp
+//   la s0, _check_8bit_0_table
+//   check_8bit_0 s0, gp, 0
+//   check_8bit_0 s0, tp, 1
+//   add gp, gp, tp
+//   check_8bit_0 s0, tp, 2
+//   add gp, gp, tp
+//   check_8bit_0 s0, tp, 3
+//   add gp, gp, tp
 
-  la tp, _same_result_table
-  add tp, tp, gp
-  lbu \rd, (tp)
+//   la tp, _same_result_table
+//   add tp, tp, gp
+//   lbu \rd, (tp)
 
-  POP(s0, 1)
-.endm
+//   POP(s0, 1)
+// .endm
 
 #define SET_BRANCH_LABEL(name) concat(name, _\@\target):
 
@@ -383,146 +384,146 @@ LABEL_NAME:
   branch_resolve \target, 0, \@
 .endm
 
-.macro getshamt rd, rs1
-  sb \rs1, SP_VAR_BYTE(VAR_B, 0)
-  lbu \rd, SP_VAR_BYTE(VAR_B, 0)
-  la tp, (_and8_table + (0x1f << 8))
-  add \rd, \rd, tp
-  lbu \rd, (\rd)
-.endm
+// .macro getshamt rd, rs1
+//   sb \rs1, SP_VAR_BYTE(VAR_B, 0)
+//   lbu \rd, SP_VAR_BYTE(VAR_B, 0)
+//   la tp, (_and8_table + (0x1f << 8))
+//   add \rd, \rd, tp
+//   lbu \rd, (\rd)
+// .endm
 
-.macro get_shift_byte_in_tp base, boffset
-  lbu tp, SP_VAR_BYTE(VAR_A, \boffset)
-  slli tp, tp, 2 # word per entry
-  add tp, \base, tp
-  lw tp, (tp)
-.endm
+// .macro get_shift_byte_in_tp base, boffset
+//   lbu tp, SP_VAR_BYTE(VAR_A, \boffset)
+//   slli tp, tp, 2 # word per entry
+//   add tp, \base, tp
+//   lw tp, (tp)
+// .endm
 
-.macro sll rd, rs1, rs2
-  SET_DEBUG_LABEL(sll)
-  PUSH(s0, 1)
+// .macro sll rd, rs1, rs2
+//   SET_DEBUG_LABEL(sll)
+//   PUSH(s0, 1)
 
-  sw \rs1, SP_VAR(VAR_A)
-  getshamt s0, \rs2
-  sw x0, SP_VAR(VAR_B)
-  sb s0, SP_VAR_BYTE(VAR_B, 1)
-  lw s0, SP_VAR(VAR_B)  # s0 = s0 << 8
-  slli s0, s0, 2  # word per entry
-  la tp, _sll8_table
-  add s0, s0, tp
+//   sw \rs1, SP_VAR(VAR_A)
+//   getshamt s0, \rs2
+//   sw x0, SP_VAR(VAR_B)
+//   sb s0, SP_VAR_BYTE(VAR_B, 1)
+//   lw s0, SP_VAR(VAR_B)  # s0 = s0 << 8
+//   slli s0, s0, 2  # word per entry
+//   la tp, _sll8_table
+//   add s0, s0, tp
 
-  sw x0, SP_VAR(VAR_B)
-  get_shift_byte_in_tp s0, 3
-  sb tp, SP_VAR_BYTE(VAR_B, 3)
-  lw gp, SP_VAR(VAR_B)  # gp = tp << 24
+//   sw x0, SP_VAR(VAR_B)
+//   get_shift_byte_in_tp s0, 3
+//   sb tp, SP_VAR_BYTE(VAR_B, 3)
+//   lw gp, SP_VAR(VAR_B)  # gp = tp << 24
 
-  get_shift_byte_in_tp s0, 2
-  sh tp, SP_VAR_BYTE(VAR_B, 2)
-  lw tp, SP_VAR(VAR_B)  # tp = tp << 16
-  add gp, gp, tp
+//   get_shift_byte_in_tp s0, 2
+//   sh tp, SP_VAR_BYTE(VAR_B, 2)
+//   lw tp, SP_VAR(VAR_B)  # tp = tp << 16
+//   add gp, gp, tp
 
-  get_shift_byte_in_tp s0, 1
-  slli tp, tp, 8
-  add gp, gp, tp
+//   get_shift_byte_in_tp s0, 1
+//   slli tp, tp, 8
+//   add gp, gp, tp
 
-  get_shift_byte_in_tp s0, 0
+//   get_shift_byte_in_tp s0, 0
 
-  POP(s0, 1)
-  add \rd, gp, tp
-.endm
+//   POP(s0, 1)
+//   add \rd, gp, tp
+// .endm
 
-.macro shift_right_template rd, rs1, rs2, is_arith, is_rs2_imm
-  .if \is_rs2_imm == 1 && \rs2 == 24  # fast path
-    .if \is_arith == 1
-      sw \rs1, SP_VAR(VAR_A)
-      la tp, (_sra8_table + (\rs2 << 10))
-      lbu \rd, SP_VAR_BYTE(VAR_A, 3)
-      slli \rd, \rd, 2 # word per entry
-      add tp, tp, \rd
-      lw \rd, (tp)
-    .else
-      getbyte \rd, \rs1, 3
-    .endif
-    .exitm
-  .endif
+// .macro shift_right_template rd, rs1, rs2, is_arith, is_rs2_imm
+//   .if \is_rs2_imm == 1 && \rs2 == 24  # fast path
+//     .if \is_arith == 1
+//       sw \rs1, SP_VAR(VAR_A)
+//       la tp, (_sra8_table + (\rs2 << 10))
+//       lbu \rd, SP_VAR_BYTE(VAR_A, 3)
+//       slli \rd, \rd, 2 # word per entry
+//       add tp, tp, \rd
+//       lw \rd, (tp)
+//     .else
+//       getbyte \rd, \rs1, 3
+//     .endif
+//     .exitm
+//   .endif
 
 
-  PUSH(s0, 1)
+//   PUSH(s0, 1)
 
-  sw \rs1, SP_VAR(VAR_A)
-  .if \is_rs2_imm == 0
-    getshamt s0, \rs2
-    sw x0, SP_VAR(VAR_B)
-    sb s0, SP_VAR_BYTE(VAR_B, 1)
-    lw s0, SP_VAR(VAR_B)  # s0 = s0 << 8
-    slli s0, s0, 2  # word per entry
-    la tp, _srl8_table
-    add s0, s0, tp
-  .else
-    la s0, (_srl8_table + (\rs2 << 10))
-  .endif
+//   sw \rs1, SP_VAR(VAR_A)
+//   .if \is_rs2_imm == 0
+//     getshamt s0, \rs2
+//     sw x0, SP_VAR(VAR_B)
+//     sb s0, SP_VAR_BYTE(VAR_B, 1)
+//     lw s0, SP_VAR(VAR_B)  # s0 = s0 << 8
+//     slli s0, s0, 2  # word per entry
+//     la tp, _srl8_table
+//     add s0, s0, tp
+//   .else
+//     la s0, (_srl8_table + (\rs2 << 10))
+//   .endif
 
-  # result = ((x3 << 24) | (x2 << 16) | (x1 << 8) | x0) >> a
-  # let y be the values fetched from the table, then
-  #   y3 = (x3 << 24 >> a)
-  #   y2 = (x2 << 24 >> a)
-  #   y1 = (x1 << 24 >> a)
-  #   y0 = (x0 << 24 >> a)
-  # result = y3 | (y2 >> 8) | (y1 >> 16) | (y0 >> 24)
-  #        = { y33, y32, y31, y30 } |
-  #               { y23, y22, y21 } |
-  #                    { y13, y12 } |
-  #                         { y03 }
+//   # result = ((x3 << 24) | (x2 << 16) | (x1 << 8) | x0) >> a
+//   # let y be the values fetched from the table, then
+//   #   y3 = (x3 << 24 >> a)
+//   #   y2 = (x2 << 24 >> a)
+//   #   y1 = (x1 << 24 >> a)
+//   #   y0 = (x0 << 24 >> a)
+//   # result = y3 | (y2 >> 8) | (y1 >> 16) | (y0 >> 24)
+//   #        = { y33, y32, y31, y30 } |
+//   #               { y23, y22, y21 } |
+//   #                    { y13, y12 } |
+//   #                         { y03 }
 
-  get_shift_byte_in_tp s0, 2
-  sw tp, SP_VAR(VAR_B)
-  get_shift_byte_in_tp s0, 1
-  sw tp, SP_VAR(VAR_C)
-  get_shift_byte_in_tp s0, 0
-  sw tp, SP_VAR(VAR_D)
+//   get_shift_byte_in_tp s0, 2
+//   sw tp, SP_VAR(VAR_B)
+//   get_shift_byte_in_tp s0, 1
+//   sw tp, SP_VAR(VAR_C)
+//   get_shift_byte_in_tp s0, 0
+//   sw tp, SP_VAR(VAR_D)
 
-  # y3 is added in the end
-  # compute byte 0
-  lbu gp, SP_VAR_BYTE(VAR_B, 1)
-  lbu tp, SP_VAR_BYTE(VAR_C, 2)
-  add gp, gp, tp
-  lbu tp, SP_VAR_BYTE(VAR_D, 3)
-  add gp, gp, tp
-  sw gp, SP_VAR(VAR_D)  # no carry during addition
+//   # y3 is added in the end
+//   # compute byte 0
+//   lbu gp, SP_VAR_BYTE(VAR_B, 1)
+//   lbu tp, SP_VAR_BYTE(VAR_C, 2)
+//   add gp, gp, tp
+//   lbu tp, SP_VAR_BYTE(VAR_D, 3)
+//   add gp, gp, tp
+//   sw gp, SP_VAR(VAR_D)  # no carry during addition
 
-  # compute byte 1
-  lbu gp, SP_VAR_BYTE(VAR_B, 2)
-  lbu tp, SP_VAR_BYTE(VAR_C, 3)
-  add gp, gp, tp
-  sb gp, SP_VAR_BYTE(VAR_D, 1)  # no carry during addition
+//   # compute byte 1
+//   lbu gp, SP_VAR_BYTE(VAR_B, 2)
+//   lbu tp, SP_VAR_BYTE(VAR_C, 3)
+//   add gp, gp, tp
+//   sb gp, SP_VAR_BYTE(VAR_D, 1)  # no carry during addition
 
-  # compute byte 2
-  lbu gp, SP_VAR_BYTE(VAR_B, 3)
-  sb gp, SP_VAR_BYTE(VAR_D, 2)
+//   # compute byte 2
+//   lbu gp, SP_VAR_BYTE(VAR_B, 3)
+//   sb gp, SP_VAR_BYTE(VAR_D, 2)
 
-  # combine byte 2~0
-  lw gp, SP_VAR(VAR_D)
+//   # combine byte 2~0
+//   lw gp, SP_VAR(VAR_D)
 
-  .if \is_arith == 1
-    la tp, (_sra8_table - _srl8_table)
-    add s0, s0, tp
-  .endif
+//   .if \is_arith == 1
+//     la tp, (_sra8_table - _srl8_table)
+//     add s0, s0, tp
+//   .endif
 
-  get_shift_byte_in_tp s0, 3
+//   get_shift_byte_in_tp s0, 3
 
-  POP(s0, 1)
-  add \rd, gp, tp
-.endm
+//   POP(s0, 1)
+//   add \rd, gp, tp
+// .endm
 
-.macro srl rd, rs1, rs2
-  SET_DEBUG_LABEL(srl)
-  shift_right_template \rd, \rs1, \rs2, 0, 0
-.endm
+// .macro srl rd, rs1, rs2
+//   SET_DEBUG_LABEL(srl)
+//   shift_right_template \rd, \rs1, \rs2, 0, 0
+// .endm
 
-.macro sra rd, rs1, rs2
-  SET_DEBUG_LABEL(sra)
-  shift_right_template \rd, \rs1, \rs2, 1, 0
-.endm
+// .macro sra rd, rs1, rs2
+//   SET_DEBUG_LABEL(sra)
+//   shift_right_template \rd, \rs1, \rs2, 1, 0
+// .endm
 
 
 // .macro slli rd, rs1, imm
@@ -563,44 +564,44 @@ LABEL_NAME:
 //   shift_right_template \rd, \rs1, \imm, 1, 1
 // .endm
 
-def_itype(slti, slt)
-def_itype(sltiu, sltu)
-def_itype(andi, and)
-def_itype(ori, or)
-def_itype(xori, xor)
+// def_itype(slti, slt)
+// def_itype(sltiu, sltu)
+// def_itype(andi, and)
+// def_itype(ori, or)
+// def_itype(xori, xor)
 
-.macro lb rd, addr
-  sw x0, SP_VAR(VAR_A)
-  lbu \rd, \addr
-  sb \rd, SP_VAR_BYTE(VAR_A, 3)
-  lw \rd, SP_VAR(VAR_A)
-  srai \rd, \rd, 24
-.endm
+// .macro lb rd, addr
+//   sw x0, SP_VAR(VAR_A)
+//   lbu \rd, \addr
+//   sb \rd, SP_VAR_BYTE(VAR_A, 3)
+//   lw \rd, SP_VAR(VAR_A)
+//   srai \rd, \rd, 24
+// .endm
 
-.macro lhu rd, addr
-  sw x0, SP_VAR(VAR_A)
-  lbu tp, 1 + \addr
-  sb tp, SP_VAR_BYTE(VAR_A, 1)
-  lw tp, SP_VAR(VAR_A)
-  lbu \rd, 0 + \addr
-  add \rd, \rd, tp
-.endm
+// .macro lhu rd, addr
+//   sw x0, SP_VAR(VAR_A)
+//   lbu tp, 1 + \addr
+//   sb tp, SP_VAR_BYTE(VAR_A, 1)
+//   lw tp, SP_VAR(VAR_A)
+//   lbu \rd, 0 + \addr
+//   add \rd, \rd, tp
+// .endm
 
-.macro lh rd, addr
-  sw x0, SP_VAR(VAR_A)
-  lbu gp, 1 + \addr
-  sb gp, SP_VAR_BYTE(VAR_A, 3)
-  lw gp, SP_VAR(VAR_A)
-  srai gp, gp, 16
-  lbu \rd, 0 + \addr
-  add \rd, \rd, gp
-.endm
+// .macro lh rd, addr
+//   sw x0, SP_VAR(VAR_A)
+//   lbu gp, 1 + \addr
+//   sb gp, SP_VAR_BYTE(VAR_A, 3)
+//   lw gp, SP_VAR(VAR_A)
+//   srai gp, gp, 16
+//   lbu \rd, 0 + \addr
+//   add \rd, \rd, gp
+// .endm
 
-.macro sh rs, addr
-  sb \rs, 0 + \addr
-  getbyte tp, \rs, 1
-  sb tp, 1 + \addr
-.endm
+// .macro sh rs, addr
+//   sb \rs, 0 + \addr
+//   getbyte tp, \rs, 1
+//   sb tp, 1 + \addr
+// .endm
 
 # RISC-V pseudo-instruction
 
@@ -629,33 +630,34 @@ def_itype(xori, xor)
 .endm
 
 .macro not rd, rs1
-  PUSH(gp, 3)
-  sw \rs1, SP_VAR(VAR_C)
-  la tp, (_xor8_table + (0xff << 8))
+  xori \rd, \rs1, -1
+  // PUSH(gp, 3)
+  // sw \rs1, SP_VAR(VAR_C)
+  // la tp, (_xor8_table + (0xff << 8))
 
-  lbu gp, SP_VAR_BYTE(VAR_C, 3)
-  add gp, gp, tp
-  lbu gp, (gp)
-  sb  gp, SP_VAR_BYTE(VAR_D, 3)
+  // lbu gp, SP_VAR_BYTE(VAR_C, 3)
+  // add gp, gp, tp
+  // lbu gp, (gp)
+  // sb  gp, SP_VAR_BYTE(VAR_D, 3)
 
-  lbu gp, SP_VAR_BYTE(VAR_C, 2)
-  add gp, gp, tp
-  lbu gp, (gp)
-  sb  gp, SP_VAR_BYTE(VAR_D, 2)
+  // lbu gp, SP_VAR_BYTE(VAR_C, 2)
+  // add gp, gp, tp
+  // lbu gp, (gp)
+  // sb  gp, SP_VAR_BYTE(VAR_D, 2)
 
-  lbu gp, SP_VAR_BYTE(VAR_C, 1)
-  add gp, gp, tp
-  lbu gp, (gp)
-  sb  gp, SP_VAR_BYTE(VAR_D, 1)
+  // lbu gp, SP_VAR_BYTE(VAR_C, 1)
+  // add gp, gp, tp
+  // lbu gp, (gp)
+  // sb  gp, SP_VAR_BYTE(VAR_D, 1)
 
-  lbu gp, SP_VAR_BYTE(VAR_C, 0)
-  add gp, gp, tp
-  lbu gp, (gp)
-  sb  gp, SP_VAR_BYTE(VAR_D, 0)
+  // lbu gp, SP_VAR_BYTE(VAR_C, 0)
+  // add gp, gp, tp
+  // lbu gp, (gp)
+  // sb  gp, SP_VAR_BYTE(VAR_D, 0)
 
-  POP(gp, 3)
+  // POP(gp, 3)
 
-  lw \rd, SP_VAR(VAR_D)
+  // lw \rd, SP_VAR(VAR_D)
 .endm
 
 .macro neg rd, rs
@@ -667,7 +669,8 @@ def_itype(xori, xor)
 .endm
 
 .macro sltz rd, rs
-  msb \rd, \rs
+  slt \rd, \rs, x0
+  // msb \rd, \rs
 .endm
 
 .macro sgt rd, rs1, rs2
@@ -682,12 +685,12 @@ def_itype(xori, xor)
   slt \rd, x0, \rs
 .endm
 
-#if 0
-# already defined above
+// #if 0
+// # already defined above
 .macro seqz rd, rs
   sltiu \rd, \rs, 1
 .endm
-#endif
+// #endif
 
 .macro beqz rs, offset
   beq \rs, x0, \offset
